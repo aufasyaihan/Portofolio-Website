df2372652444638083f770b8b19d3d1d2f8ac034
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-05-31 00:25:17 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return(
+        <div className="bg-slate-900 h-[1000px]">
+            
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return (
+        <nav className="bg-indigo-900 w-full font-poppin fixed top-0">
+            <h1 className="text-3xl text-white font-bold p-4">Portofolio</h1>
+        </nav>
+    )
+}
