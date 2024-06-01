d1a4909b233a91949a6bfad892493939ccd7ef17
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-02 01:38:26 +0700

    last

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import ExperienceLists from "./ExperienceLists";
 export default function Experience() {
   return (
     <>
-      <div className="relative bg-darkBlue-900 px-10 md:px-24 py-10 rounded-t-[50px] shadow-lg text-white flex flex-col gap-5">
+      <div className="relative bg-darkBlue-900 px-10 md:px-24 py-10 rounded-xl shadow-lg text-white flex flex-col gap-5 overflow-hidden">
         <h1 className="font-bold text-4xl text-gold underline underline-offset-8 decoration-gold">
           Experiences
         </h1>
@@ -23,7 +23,7 @@ export default function Experience() {
           </ol>
         </div>
         <h2 className="text-2xl">Organization Experiences</h2>
-        <div className="pl-8 -mt-4">
+        <div className="pl-8 -mt-4 z-10">
           <ol className="md:text-xl">
             {ORGANIZATIONS_EXPERIENCES.map((exp) => {
               return (
@@ -38,8 +38,8 @@ export default function Experience() {
             })}
           </ol>
         </div>
-        <div className="absolute top-28 right-0 p-12 blur-3xl rounded-full bg-gold object-cover animate-pulse"></div>
-        <div className="absolute bottom-28 left-0 p-10 blur-3xl rounded-full bg-sky-700 object-cover animate-pulse"></div>
+        <div className="absolute top-28 right-0 p-16 blur-3xl rounded-full bg-gold object-cover animate-pulse"></div>
+        <div className="absolute bottom-0 left-0 p-20 blur-3xl rounded-full bg-sky-700 object-cover animate-pulse"></div>
       </div>
       {/* <div className="w-full h-32 bg-gradient-to-b from-darkBlue-500 "></div> */}
     </>
