3edf90a54627e1750f82c554e542fffcecc865af
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-02 01:06:02 +0700

    added active button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable react/prop-types */
-export default function Button({children, ...props}) {
-    return (
-        <button className="px-4 py-2 rounded-full border-2 border-gray-400 hover:bg-darkBlue-900 hover:border-darkBlue-500 hover:text-white" {...props}>
-            {children}
-        </button>
-    )
+export default function Button({ children, isActive, ...props }) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-full border-2 border-gray-400 hover:bg-darkBlue-900 hover:border-darkBlue-500 hover:text-white transition-all ease-in ${
+        isActive ? `bg-darkBlue-900 text-white` : ``
+      }`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -4,36 +4,60 @@ import Button from "./Button";
 import { useState } from "react";
 
 export default function Certificate() {
-    const [filter, setFilter] = useState(CERTIFICATE);
-    function handleClick(type){
-        if(type === "organization"){
-            type = "organization";
-        } else if(type === "committee"){
-            type = "committee";
-        } else if(type === "work"){
-            type = "work";
-        } else if(type === "course & bootcamp"){
-            type = "course & bootcamp";
-        } else {
-            type = null;
-        }
-
-        setFilter(CERTIFICATE.filter((item) => item.type === type || type === null));
+  const [filter, setFilter] = useState(CERTIFICATE);
+  const [isActive, setIsActive] = useState("all");
+  function handleClick(type) {
+    setIsActive(type);
+    if (
+      type === "organization" ||
+      type === "committee" ||
+      type === "work" ||
+      type === "course & bootcamp"
+    ) {
+      setFilter(CERTIFICATE.filter((item) => item.type === type));
+    } else {
+      setFilter(CERTIFICATE);
     }
+  }
   return (
     <div className="py-10 sm:px-10 md:px-24 bg-white">
       <h1 className="font-bold text-4xl underline underline-offset-8 decoration-sky-500 px-10">
         Certification
       </h1>
       <div className="relative my-5">
-        <div className="flex gap-2 px-12 py-4">
-          <Button onClick={() => handleClick("all")}>All</Button>
-          <Button onClick={() => handleClick("organization")}>Organization</Button>
-          <Button onClick={() => handleClick("committee")}>Committee</Button>
-          <Button onClick={() => handleClick("work")}>Work</Button>
-          <Button onClick={() => handleClick("course & bootcamp")}>Course & Bootcamp</Button>
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:flex text-xs md:text-md gap-2 px-12 py-4">
+          <Button
+            onClick={() => handleClick("all")}
+            isActive={isActive === "all"}
+          >
+            All
+          </Button>
+          <Button
+            onClick={() => handleClick("organization")}
+            isActive={isActive === "organization"}
+          >
+            Organization
+          </Button>
+          <Button
+            onClick={() => handleClick("committee")}
+            isActive={isActive === "committee"}
+          >
+            Committee
+          </Button>
+          <Button
+            onClick={() => handleClick("work")}
+            isActive={isActive === "work"}
+          >
+            Work
+          </Button>
+          <Button
+            onClick={() => handleClick("course & bootcamp")}
+            isActive={isActive === "course & bootcamp"}
+          >
+            Course & Bootcamp
+          </Button>
         </div>
-        <SliderCertif sertif={filter}/>
+        <SliderCertif sertif={filter} />
       </div>
     </div>
   );
