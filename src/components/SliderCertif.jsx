b6b432dbff30bfffc6244b76c3d508c2110348e8
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-10 12:32:25 +0700

    fixed styling on certifications

--- src/components/SliderCertif.jsx
+++ src/components/SliderCertif.jsx
@@ -34,21 +34,21 @@ export default function SliderCertif({ sertif }) {
             <a
               href={certif.link}
               target="_blank"
-              className="flex flex-col border-2 rounded-lg h-96 sm:h-[450px] bg-white overflow-hidden mb-10 mx-12 shadow-lg"
+              className="flex flex-col border-2 rounded-lg h-96 sm:h-[450px] bg-white overflow-hidden mb-10 mx-12 shadow-lg group"
             >
-              <div className="sm:h-80 h-full w-full">
+              <div className="sm:h-80 h-full w-full overflow-hidden">
                 <img
                   src={certif.img}
                   alt={certif.name}
-                  className="h-48 w-full object-cover"
+                  className="h-fit sm:h-full w-full object-cover group-hover:scale-110 transition-all duration-200 ease-in-out"
                 />
               </div>
               <div className="p-4 h-full w-full flex flex-col justify-between">
                 <div>
-                  <h1 className="font-bold text-md md:text-lg">
+                  <h1 className="font-bold text-md sm:text-xl">
                     {certif.name}
                   </h1>
-                  <p className="text-gray-600 text-xs sm:text-md md:text-lg">
+                  <p className="text-gray-600 text-xs sm:text-lg">
                     {certif.institution}
                   </p>
                 </div>
