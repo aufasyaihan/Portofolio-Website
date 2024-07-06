df5903b6e90b6fde55092c8f564620ce572ce7ff
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-06 13:59:20 +0700

    fixed threshold and outsourced section to different file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import About from "./components/About";
 import Certificate from "./components/Certificates";
@@ -7,9 +6,8 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import { motion } from "framer-motion";
 import Loading from "./UI/Loading";
-import { useInView } from "react-intersection-observer";
+import { Section } from "./components/Section";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,32 +15,6 @@ function App() {
   useEffect(() => {
     setTimeout(() => setIsLoading(false), 2000);
   }, []);
-
-  const variants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-  };
-
-  const Section = ({ id, children }) => {
-    const [ref, inView] = useInView({
-      triggerOnce: true,
-      threshold: 0.5,
-    });
-
-    return (
-      <motion.section
-        ref={ref}
-        initial="hidden"
-        animate={inView ? "visible" : "hidden"}
-        variants={variants}
-        transition={{ duration: 1 }}
-        className="pt-[85px] -mt-10"
-        id={id}
-      >
-        {children}
-      </motion.section>
-    );
-  };
 
   return (
     <>
---
+++ src/components/Section.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
+export function Section({ id, children }) {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <motion.section
+      ref={ref}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      variants={variants}
+      transition={{ duration: 1 }}
+      className="pt-[85px] -mt-10"
+      id={id}
+    >
+      {children}
+    </motion.section>
+  );
+}
