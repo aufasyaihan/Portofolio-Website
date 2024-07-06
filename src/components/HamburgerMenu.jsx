329ddf524219ffac387ef5ad820f9febcb83fb32
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-07-07 01:10:43 +0700

    added animation on hamburgermenu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,12 +1,27 @@
 /* eslint-disable react/prop-types */
+import { motion } from "framer-motion";
+
 export default function HamburgerMenu({ children, isOpen }) {
   return (
-    <div
+    <motion.div
+      initial={{ height: 0, opacity: 0 }}
+      animate={{
+        height: isOpen ? "50vh" : 0,
+        opacity: isOpen ? 1 : 0,
+      }}
       className={`absolute text-end top-0 right-0 bg-black bg-opacity-75 text-white pl-28 pr-6 pt-20 pb-10 w-full backdrop-blur-md ${
         isOpen ? `block` : `hidden`
       } transision-all ease-in`}
     >
-      <ol className="flex flex-col gap-6">{children}</ol>
-    </div>
+      <motion.div
+        initial={{ x: 20, opacity: 0 }}
+        animate={{
+          opacity: isOpen ? 1 : 0,
+          x: isOpen ? 0 : 20,
+        }}
+      >
+        <ol className="flex flex-col gap-6">{children}</ol>
+      </motion.div>
+    </motion.div>
   );
 }
