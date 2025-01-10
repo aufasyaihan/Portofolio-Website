674174d227ffe8dd8bfac7ff81018fbcd966d86d
Aufa Syaihan Azzahidi <aufa.azzahidi@gmail.com>
2025-01-10 22:54:06 +0700

    changed cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ export default function Hero() {
           Your favorite <span className="text-gold">Web</span> Developer
         </p>
         <a
-          href="https://drive.google.com/file/d/1w5Bz6bwn7kRq1TIf9puioVCp6zcQHctB/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1BYVn4HswuTr3mDHj2dmE4IhowCoZmxO1/view?usp=sharing"
           target="_blank"
           className="absolute flex gap-1 items-center px-4 py-2 w-fit bg-gold text-darkBlue-900 mt-3 rounded-full hover:shadow-custom-brightGold transition-all duration-200 ease-in-out z-40"
         >
